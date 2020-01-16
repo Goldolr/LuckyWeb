faaa4087308e930abab4805e8a2b8afa5e7e0d8c
OLIVER ANDRES GUAMAN LOPEZ <oaguamanl@utn.edu.ec>
2020-01-16 17:02:43 -0500

    initial commit

---
+++ LuckyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LuckyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
