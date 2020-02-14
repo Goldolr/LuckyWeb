719f03a31699efcfb2f95426d8bee757e5d0317b
OLIVER ANDRES GUAMAN LOPEZ <oaguamanl@utn.edu.ec>
2020-02-13 22:27:46 -0500

    Add Docker

--- LuckyWeb/obj/Debug/netcoreapp2.2/LuckyWeb.AssemblyInfo.cs
+++ LuckyWeb/obj/Debug/netcoreapp2.2/LuckyWeb.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7026bb10-97b5-4e17-8612-1fb8bc68688c")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("LuckyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
